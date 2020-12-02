be149af9fed63f08e5b219e0bcc154ca50c8a4f3
demiler <seerpout@gmail.com>
2020-12-02 20:06:03 +0300

    added more tool buttons. added actions for clicks on tiles. added sync with db on server. actions for favorite and hidden. config updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import { LitElement, css, html } from 'lit-element'
 import { style } from '../css/style.js'
 import { api } from '../js/api.js'
+const { File } = require('../../back/file.js');
 
 class Explorer extends LitElement {
   static get styles() {
@@ -11,23 +12,24 @@ class Explorer extends LitElement {
     return {
       files: { type: Array },
       state: { type: String },
+      hider: { type: Boolean },
+      favonly: { type: Boolean },
+      showhid: { type: Boolean },
     };
   }
 
   constructor() {
     super();
+    this.activeFile = undefined;
     this.files = [];
     this.state = "waiting-for-files";
-    //this.state = "buttons";
+    this.hider = false;
+    this.favonly = false;
+    this.showhid = false;
+    this.lastDump = Date.now();
 
-    api.on('rebase', (files) => {
-      this.files = files.map(file => {
-        return {
-          basename: file[0].split(/(\\|\/)/g).pop(),
-          path: file[0],
-          data: file[1]
-        }
-      });
+    api.on('rebase', (db) => {
+      this.files = db.map(data => data[1]);
       this.state = "display-files";
     });
 
@@ -44,6 +46,10 @@ class Explorer extends LitElement {
 
     api.on('new-file', (file) => {
       this.files.push({
+        hidden: false,
+        display: true,
+        favorite: false,
+        active: false,
         path: file.path,
         data: file.data,
         basename: file.path.split(/(\\|\/)/g).pop()
@@ -55,7 +61,23 @@ class Explorer extends LitElement {
       this.buttons.forEach(button =>
         button.value = conf[button.confname]
       );
+      if (this.files.length === 0)
+        return;
+
+      this.activeFile = this.files.find(file => file.path === conf['source_image']);
+      if (this.activeFile === undefined) {
+        [this.activeFile] = this.files;
+        this.updateImage(this.activeFile);
+      }
+      else
+        this.activeFile.active = true;
+
       this.requestUpdate();
+    });
+
+    api.on('db-dumped', () => {
+      this.lastDump = Date.now();
+      console.log(this.lastDump);
     });
 
     ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
@@ -89,7 +111,16 @@ class Explorer extends LitElement {
     ];
 
     setTimeout(() => api.send('reqFiles'), 100);
-    setTimeout(() => api.send('reqConf'), 100);
+    setTimeout(() => api.send('reqConf'), 200);
+
+    const updater = setInterval(() => this.requestUpdate(), 60000);
+  }
+
+  dumpTime() {
+    let diff = (Date.now() - this.lastDump) / 60000; //to min
+    if (diff < 1)
+      return 'recently';
+    return Number(diff).toFixed(0) + ' mins ago';
   }
 
   render() {
@@ -105,8 +136,25 @@ class Explorer extends LitElement {
 
   renderControls() {
     return html`
-      <div class='ctrl-container'>
+      <div class="ctrl-container ${this.hider ? 'hider' : ''}">
         ${this.renderButtons()}
+        <div class='tools-container'>
+          <button class='btn-push btn-hider ${this.hider}' 
+          @click=${() => this.hider = !this.hider}>hider</button>
+
+          <button class='btn-push btn-showhid ${this.showhid}' 
+          @click=${() => this.showhid = !this.showhid}>
+            show hidden (${this.files.filter(file => this.isHidden(file)).length})
+          </button>
+
+          <button class='btn-push btn-favorite ${this.favonly}' 
+          @click=${() => this.favonly = !this.favonly}>favorite only</button>
+
+          <button class='btn-push btn-dump false' 
+                  @click=${() => api.send('dump-db')}>
+                  dump on disk (${this.dumpTime()})
+          </button>
+        </div>
         ${this.renderFiles()}
       </div>
     `;
@@ -116,7 +164,7 @@ class Explorer extends LitElement {
     return html`
       <div class='conf-buttons'>
         ${this.buttons.map(button => html`
-          <button class='btn-conf ${button.value}'
+          <button class='btn-push btn-conf ${button.value}'
            @click=${() => this.updateConfig(button)}
            >${button.label}</button>
         `)}
@@ -132,16 +180,68 @@ class Explorer extends LitElement {
 
   renderFiles() {
     return html`
-      <div class="files-container">
+      <div class="files-container" .hidden=${this.showhid}>
         ${this.files.map(file => html`
-          <div class="file-wrap"
-            @click=${() => this.updateImage(file)}>
+          <div class="file-wrap ${file.active ? 'active' : ''}
+          ${file.favorite ? 'favorite' : ''}"
+           @click=${(e) => this.clickImage(e, file)}
+           .hidden=${this.isHidden(file)}
+           >
             <img src="${file.data}">
             <span>${file.basename}</span>
+            <button class="btn-fav" name="btn-fav"
+            @click=${() => this.addToFav(file)}>⭐</button>
           </div>
         `)}
+        ${this.files.filter(file => this.isHidden(file)).length === this.files.length ?
+          html`<div>Empty! Try changing filters</div>`
+          : html``}
       </div>
     `;
+  }
+
+  addToFav(img) {
+    img.favorite = !img.favorite;
+    api.send('toggle-fav', { path: img.path, value: img.favorite });
+    this.requestUpdate();
+  }
+
+  isHidden(img) {
+    return img.hidden || (this.favonly && !img.favorite);
+  }
+
+  hideImage(img) {
+    img.hidden = !img.hidden;
+    api.send('toggle-hid', { path: img.path, value: img.hidden });
+  }
+
+  clickImage(e, img) {
+    if (e.target.name === "btn-fav")
+      return;
+    if (e.ctrlKey)
+      this.addToFav(img)
+    else if (this.hider || e.shiftKey)
+      this.hideImage(img)
+    else
+      this.updateImage(img);
+    this.requestUpdate();
+  }
+
+  updateImage(img) {
+    if (this.activeFile)
+      this.activeFile.active = false;
+    this.activeFile = img;
+    this.activeFile.active = true;
+
+    api.send('upd-img', img.path);
+    console.log('updating image', img.basename);
+  }
+
+  updateConfig(button) {
+    button.value = !button.value;
+    api.send('upd-config', { name: button.confname, value: button.value });
+    console.log('updating config', button);
+    this.requestUpdate();
   }
 
   loadFile(e) {
@@ -156,6 +256,7 @@ class Explorer extends LitElement {
           timer = setTimeout(() => this.classList.remove('unsupp'), 1000);
         continue;
       }
+
       new Promise((res, rej) => {
         const reader = new FileReader();
         reader.readAsDataURL(files[i]);
@@ -165,17 +266,6 @@ class Explorer extends LitElement {
     }
   }
 
-  updateImage(img) {
-    api.send('upd-img', img.path);
-    console.log('updating image', img.basename);
-  }
-
-  updateConfig(button) {
-    button.value = !button.value;
-    api.send('upd-config', { name: button.confname, value: button.value });
-    console.log('updating config', button);
-    this.requestUpdate();
-  }
 };
 
 customElements.define('le-exp', Explorer);
