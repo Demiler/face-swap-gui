b483d9a85e30a6a54436d81e21d31d2dab0995a8
demiler <seerpout@gmail.com>
2020-12-02 20:06:40 +0300

    File class added and icons

---
+++ back/file.js
@@ -0,0 +1,26 @@
+class File {
+  constructor() {
+    this.path = undefined;
+    this.data = undefined;
+    this.basename = undefined;
+    this.hidden = false;
+    this.favorite = false;
+  }
+
+  lite() {
+    return {
+      path: this.path,
+      hidden: this.hidden,
+      favorite: this.favorite,
+    }
+  }
+
+  fromLite(lt) {
+    this.path = lt.path;
+    this.hidden = lt.hidden;
+    this.favorite = lt.favorite;
+    this.basename = lt.path.split(/(\\|\/)/g).pop();
+  }
+};
+
+module.exports.File = File;
